ae3a25146908ade699595fe60884a89b026be1f2
Pyrite32 <patrick@method93.com>
2024-03-28 15:44:13 -0400

    login page

--- GatorTask_client/src/Login.jsx
+++ GatorTask_client/src/Login.jsx
@@ -21,52 +21,54 @@ function Login() {
     }
 
     return (
-        <div className="d-flex justify-content-center align-items-center blue-background">
-            <div className="bg-white p-3 rounded w-40 ">
-                <h1>Compartments</h1>
-                <h2>Login</h2>
-
-                <form onSubmit={handleSubmit}>
-         
-                    {/* Email Field */}
-                    <div className="mb-3">
-                        <label htmlFor="email">
-                            <strong>Email</strong>
-                        </label>
-                        <input
-                            type="email"
-                            placeholder="Enter Email"
-                            autoComplete="off"
-                            name="email"
-                            className="form-control rounded-0"
-                            onChange={(e) => setEmail(e.target.value)}
-
-                        />
-                    </div>
-
-                    {/* Password Field */}
-                    <div className="mb-3">
-                        <label htmlFor="password">
-                            <strong>Password</strong>
-                        </label>
-                        <input
-                            type="password"
-                            placeholder="Enter Password"
-                            name="password"
-                            className="form-control rounded-0"
-                            onChange={(e) => setPassword(e.target.value)}
-
-                        />
-                    </div>
-
-                    <button type="submit" className="btn btn-success w-100 rounded-0">
-                        Login
-                    </button>
-                </form>
-
-              
+        <>
+            <div className="">
+                <img src='./src/assets/comp-logo.png' alt="react" className="logo"/>
             </div>
-        </div>
+            <div className="min-h-screen flex justify-end">
+                <div className="bg-slate-200 pt-10 pl-10 pr-10 pb-20 max-w-xl">
+                    <h1 className="font-noto font-bold text-4xl mb-3">Login</h1>
+
+                    <form onSubmit={handleSubmit}>
+            
+                        {/* Email Field */}
+                        <div className="mb-3">
+                            <label htmlFor="email" className="signup-label">
+                                <strong>Email</strong>
+                            </label>
+                            <input
+                                type="email"
+                                placeholder="Enter Email"
+                                autoComplete="off"
+                                name="email"
+                                className="form-control rounded-0 font-noto italic"
+                                onChange={(e) => setEmail(e.target.value)}
+
+                            />
+                        </div>
+
+                        {/* Password Field */}
+                        <div className="mb-3">
+                            <label htmlFor="password" className="signup-label">
+                                <strong>Password</strong>
+                            </label>
+                            <input
+                                type="password"
+                                placeholder="Enter Password"
+                                name="password"
+                                className="form-control rounded-0 font-noto italic"
+                                onChange={(e) => setPassword(e.target.value)}
+
+                            />
+                        </div>
+
+                        <button type="submit" className="btn font-noto btn-success button-register w-100 pt-2 pb-2 border rounded-0 border-color-blue-500">
+                            Login
+                        </button>
+                    </form>                
+                </div>
+            </div>
+        </>
     );
 }
 
